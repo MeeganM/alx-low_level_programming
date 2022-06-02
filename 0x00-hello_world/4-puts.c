027f9ce388533939f5ea0139777e35e3e027a777
MeeganM <mositoameegan@gmail.com>
2022-06-02 15:16:20 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 
 /**
  *  * main - prints the phrase "Programming is like building a multilingual
- *    puzzle
- *    Return: 0 if exited properly, non-zero otherwise
- *     */
+ *  puzzle
+ *  Return: 0 if exited properly, non-zero otherwise
+ 
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
