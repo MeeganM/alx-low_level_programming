f9141a0fd44e3967da14d7f8fd385789e696ed1b
MeeganM <mositoameegan@gmail.com>
2022-06-07 14:21:42 -0700

    task2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,24 +6,14 @@
 */
 void print_alphabet_x10(void)
 {
-	char ch;
-	int i;
+	char c, i;
 
-	i = 0;
-
-	while (i < 10)
+	for (i = 0; i <= 9; i++)
 	{
-
-	ch = 'a';
-	while (ch <= 'z')
+	for (c = 'a'; c <= 'z'; c++)
 	{
-
-	_putchar(ch);
-	ch++;
-
+	_putchar(c);
 	}
-	_putchar('\n');
-	i++;
+	_putchar(\'n');
 	}
-
 }
