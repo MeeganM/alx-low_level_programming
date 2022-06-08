008aa803b7dd5dd22fb657973972e1efb3d67634
MeeganM <mositoameegan@gmail.com>
2022-06-07 18:21:52 -0700

    task12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,6 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-	
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -20,20 +19,19 @@ void print_times_table(int n)
 					_putchar(k + '0');
 				} else if (k < 10 && j != 0)
 				{
-
 					_putchar(',');
 					_putchar(' ');
-				 	_putchar(' ');
-				 	_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
 					 _putchar(k + '0');
-				}else if (k >= 10 && k < 100)
+				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
-				}else if (k >= 100)
+				} else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -46,4 +44,3 @@ void print_times_table(int n)
 		}
 	}
 }
-				
