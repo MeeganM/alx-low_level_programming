32beca8ca96fb7989740eda4eee15d8f9a404ece
MeeganM <mositoameegan@gmail.com>
2022-06-07 18:45:04 -0700

    task13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,16 +9,15 @@
  */
 int main(void)
 {
-		int i, sum = 0;
-		
-		for(i = 0; i < 1024; i++)
-		{
-					if ((i % 3) == 0 || (i % 5) == 0)
-						sum += i;
-		}
+	int i, sum = 0;
 
-		printf("%d\n", sum);
-
-		return (0);
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	
+	printf("%d\n", sum);
+	
+	return (0);
 }
-
