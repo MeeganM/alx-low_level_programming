883f91a7eb2f4e35f8680f942b1a29207f7e9f48
MeeganM <mositoameegan@gmail.com>
2022-06-09 10:09:15 -0700

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- * function that checks for uppercase character.
  * _isupper - evalue if letter is uppercase .
- * 
- * @c: print int
+ *
+ *  * @c: print int
  * Return: Always 0.
  */
 
