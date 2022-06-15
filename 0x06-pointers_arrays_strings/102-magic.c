ce18f82dd2cd6cfa28b50670de32822b67399ad0
MeeganM <mositoameegan@gmail.com>
2022-06-15 15:28:09 -0700

    A dream doesnt become reality through magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,3 @@
+VimCrypt~03!në©w®rNô sL«¤²ÌÅîÝzrg+Ñõèts£Å°vŠÿxx‹’²Þ´O2YS¢®rõ½ðtoÛª¶1	Þ6ý0^ÎJËLÙè›s%Å;šNËWTín~XnŸÎŒZ®Ÿû´Ÿjï$ºéä˜>y³@PŸâ˜Æ%i—Á§Žñ6oÅ@ç*H\rÚEfáÃª&	7§+«M°æ#±¦ÿê6ÔÇOìÿóãÿgðÎ;-4•±ÛgŒàôEûöÀõÕ.«~~OQ¹æÃÕìAeøY#~i¦§­Â‹qä›Ê{’Ï/}óÛ“;&€v¹¾´Bkt’ûú¹–…aE}…œ8
+Un¥˜\ž˜]`jË5ZUgêOÁ‹‰…×&ÞjE<F%÷œCŒä¾=ÂÏÇ"pÝu·¡5V”	Á¡Ô1ž\RÂªù¨BÍÉEMpn U)Ó×™˜^0Bš‰–3Ï‰qgMsÇõW*S<ü¤‡@aÃüÝôÑ,“iwW€kGû
+¥QkPg¾®t–ñ~£„Áx Ýºð´¦eôÂ¯
