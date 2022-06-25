922ada123524ddfd629b8e80797b87896b04cf59
MeeganM <mositoameegan@gmail.com>
2022-06-24 17:00:27 -0700

    a program that prints the minimum number of coins to make change for an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,54 +1,38 @@
-#include <stdio.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Prints the minimum number of coins to
- * make change for an amount of money.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- * Return: If the number of arguments is not exactly one - 1.
- * Otherwise - 0.
+ * main - prints the minimum number of coins for change for an amount of money
+ * @argc: argument count
+ * @argv: argument vector, only one argument more beside program name
+ * Return: least number of coins, 0 if negative amount, 1 if amount not given
  */
-int main(int argc, char *argv[]);
+
+int main(int argc, char *argv[])
 {
-	int cents, coins = 0;
+	int n, c = 0; /* n = number and c = coins */
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	cents = atoi(argv[1]);
-
-	while (cents > 0)
+	if (argv[1][0] == '-')
 	{
-		coins++;
-		if ((cents - 25) >= 0)
+		printf("0\n");
+		return (0);
 	}
-		cents -= 25;
-		continue;
-	}
-	if ((cents - 10) >= 0)
-	{
-		cents -= 10;
-		continue;
-	}
-	if ((cents - 5) >= 0)
-	}
-		cents -= 5;
-		continue;
-	}
-	if ((cents - 2) >= 0)
-	}
-		cents -= 2;
-		continue;
-	}
-	cents--;
+	n = atoi(argv[1]);
 
-	}
+	c += n / 25;
+	n = n % 25;
+	c += n / 10;
+	n = n % 10;
+	c += n / 5;
+	n = n % 5;
+	c += n / 2;
+	n = n % 2;
+	c += n / 1;
 
-	printf("%d\n", coins);
-
+	printf("%d\n", c);
 	return (0);
 }
