ec86cd37c80e6e7e0af2b81f26a681bb8a0e9d94
MeeganM <mositoameegan@gmail.com>
2022-07-01 01:40:52 -0700

    a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -61,8 +61,7 @@ int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
-
-
+	
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
@@ -85,6 +84,9 @@ int main(int argc, char *argv[])
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
 		for (i = 0; i < len - 1; i++)
 		{
 			if (result[i])
